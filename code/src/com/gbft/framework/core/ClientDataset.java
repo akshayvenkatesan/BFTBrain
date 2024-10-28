88f2a4ccbbe4b36eb7967953026187c9866b31d6
kanavtalwar <kanavtalwar1998@gmail.com>
2024-10-27 22:34:12 -0400

    generating random txn and splitting it

--- code/src/com/gbft/framework/core/ClientDataset.java
+++ code/src/com/gbft/framework/core/ClientDataset.java
@@ -5,6 +5,8 @@ import java.util.Map;
 import java.util.Random;
 import java.util.concurrent.atomic.LongAdder;
 import java.util.stream.IntStream;
+import java.util.List;
+import java.util.ArrayList;
 
 import com.gbft.framework.data.RequestData;
 import com.gbft.framework.data.RequestData.Operation;
@@ -46,6 +48,49 @@ public class ClientDataset extends Dataset {
         default:
             break;
         }
+    }
+
+    /*
+     * Randomly Generates n transactions of the form (x,y,num) -> which means x transfers y num amount of money
+     */
+    public List<int[]> generateRandomTransactions(int n) {
+        // Create a Random object
+        Random random = new Random();
+
+        // List to store arrays
+        List<int[]> arrayList = new ArrayList<>();
+
+        // Generate 'n' arrays
+        for (int i = 0; i < n; i++) {
+            // Create an array of size 3
+            int[] array = new int[3];
+
+            // Assign random values
+            array[0] = random.nextInt(99) + 1;    // sender's key - 0th element: 1-99
+            array[1] = random.nextInt(99) + 1;    // receiver's key - 1st element: 1-99
+            array[2] = random.nextInt(100) + 1;  // Amount - 2nd element: 1-10000
+
+            // Add the array to the list
+            arrayList.add(array);
+        }
+
+        // Return the list of arrays
+        return arrayList;
+    }
+
+    public RequestData createRequestWithKeyAndVal(long reqnum, long record, long value) {
+        var operation = Operation.ADD;
+        if (value < 0) {
+            value = -1*value;
+            operation = Operation.SUB;
+        }
+        // generate read only optimization
+        if (Config.stringList("plugins.message").contains("read-only")) {
+            if (random.nextDouble() < AdvanceConfig.doubleNumber("workload.read-only-ratio")) {
+                operation = Operation.READ_ONLY;
+            }
+        }
+        return DataUtils.createRequest(reqnum, (int) record, operation, (int) value, clientId);
     }
 
     public RequestData createRequest(long reqnum) {
